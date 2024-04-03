da51670ae42122d26a65a1fdb4f26a0bf5932fe7
Blue260910 <rm99667@fiap.com.br>
2024-04-02 22:36:14 -0300

    Update user interface for code selection

--- Main.java
+++ Main.java
@@ -9,9 +9,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // Solicita ao usuário que escolha entre trabalhar com código morse ou cifra de César
-        System.out.println("Deseja utilizar código morse ou cifra de César? ");
+        System.out.println("========================================================================");
+        System.out.println("         ____                  __      ___           _         ");
+        System.out.println("        |  _ \\                 \\ \\    / (_)         | |        ");
+        System.out.println("        | |_) | ___ _ __ ___    \\ \\  / / _ _ __   __| | ___    ");
+        System.out.println("        |  _ < / _ \\ '_ ` _ \\    \\ \\/ / | | '_ \\ / _` |/ _ \\   ");
+        System.out.println("        | |_) |  __/ | | | | |    \\  /  | | | | | (_| | (_) |  ");
+        System.out.println("        |____/ \\___|_| |_| |_|     \\/   |_|_| |_|\\__,_|\\___/   ");
+        System.out.println("========================================================================");
+        System.out.println("Conversor de Código Morse e Cifra de César! ");
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("Deseja utilizar código morse ou cifra de César? | ");
+        System.out.println("-------------------------------------------------");
         System.out.println(OPCAO_1 + " - Código Morse");
         System.out.println(OPCAO_2 + " - Cifra de César");
+        System.out.println("-------------------------------------------------");
         System.out.print("Digite " + OPCAO_1 + " ou " + OPCAO_2 + ": ");
         String escolhaTipoConversao = scanner.nextLine();
 
@@ -24,10 +36,12 @@ public class Main {
                 manipularCifraCesar(scanner);
                 break;
             default:
+                System.out.println("-------------------------------------------------");
                 System.out.println("Opção inválida");
         }
 
         scanner.close();
+        
     }
 
     // Função para manipular o código Morse
@@ -35,9 +49,11 @@ public class Main {
         // Solicita ao usuário que escolha entre decriptar ou encriptar uma mensagem em código Morse
         System.out.println("Escolheu Codigo Morse");
         System.out.println("-------------------------");
-        System.out.println("Você deseja decriptar ou encriptar uma mensagem? ");
+        System.out.println("Você deseja decriptar ou encriptar uma mensagem? |");
+        System.out.println("--------------------------------------------------");
         System.out.println(OPCAO_1 + " - Decriptar");
         System.out.println(OPCAO_2 + " - Encriptar");
+        System.out.println("--------------------------------------------------");
         System.out.print("Digite " + OPCAO_1 + " ou " + OPCAO_2 + ": ");
         String escolhaTipoEncriptacao = scanner.nextLine();
 
@@ -59,9 +75,11 @@ public class Main {
         // Solicita ao usuário que escolha entre decriptar ou encriptar uma mensagem em cifra de César
         System.out.println("Escolheu Cifra de César");
         System.out.println("-------------------------");
-        System.out.println("Você deseja decriptar ou encriptar uma mensagem? ");
+        System.out.println("Você deseja decriptar ou encriptar uma mensagem? |");
+        System.out.println("--------------------------------------------------");
         System.out.println(OPCAO_1 + " - Decriptar");
         System.out.println(OPCAO_2 + " - Encriptar");
+        System.out.println("--------------------------------------------------");
         System.out.print("Digite " + OPCAO_1 + " ou " + OPCAO_2 + ": ");
         String escolhaTipoEncriptacao = scanner.nextLine();
 
